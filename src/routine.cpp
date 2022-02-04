0a40df4f03be003d5f5491e93127c23ac14262ca
Nathaniel Iskandar <nathanielri22@gmail.com>
2022-02-04 14:32:59 -0800

    Updated 02/04/22
    Bot worked for a bit while and starts to malfunction towards the end of the day.

--- src/routine.cpp
+++ src/routine.cpp
@@ -16,49 +16,39 @@ using namespace std;
 //NOT STARTING IN CORNER SQUARE
 //front left tip wheel on corner that touches 4 squares
 void aimatCorner(){
-  liftAuton(0, 10, 10);     //push lift to bottom
+liftAuton(0, 10, 10);     //push lift to bottom
   latchDown.set(1);           //open
-  moveForward(101, 100, 10);     
+  moveForward(100, 100, 10);     
   latchDown.set(0);
-  moveForward(-45, 48, 10);
+
+  //lift a little to not drag
+  liftAuton(0.5, 10, 10);
+  moveForward(-40, 55, 10);     //-45, 48,10
 
   //dropping yellow goal
-  turnClockwise(-20, 20, 10);     //turns left
-  moveForward(55, 20, 10);
-  turnClockwise(-20, 20, 10);     //turns left
-  liftAuton(120, 50, 5);  
-  //moveForward(10, 20, 10);
-  // latchDown.set(1);
-  // liftAuton(-100, 50, 5); 
-  // moveForward(-40, 30, 10);
+  turnClockwise(-20, 20, 10);     //turns left  //orignial: turnClockwise(-20, 20, 10); 
+  moveForward(75, 20, 10);
+  turnClockwise(-16, 20, 10);     //turns left
+  liftAuton(240, 50, 5);
+  wait(2, sec);  
+  moveForward(40, 20, 10);
+  liftAuton(-2, 20, 5);
+  wait(1, sec);
+  latchDown.set(1);
 
-  //grabbing personal
-  // turnClockwise(-10, 20, 10);     
-  // moveForward(101, 100, 10);
-  // latchDown.set(0);
-  // moveForward(-85, 30, 10);
-
-  //position for ramp, moving and lifting lift
-  // turnClockwise(-53, 20, 5);     
-  // liftAuton(120, 50, 5);       
-  // moveForward(20, 20, 5);
-
-  // //PLAN B FORCE IN
-  // turnClockwise(-17.5, 15, 5);   //-18,15,10
-  // moveForward(15, 10, 5);
-  // latchDown.set(1);
-
-
-  // //PLAN A REGULAR ALIGN
-  // //turn into ramp and drop
-  // turnClockwise(-14, 15, 10);    //turning against ramp   
-  // moveForward(25, 20, 10);       //get closer to drop
-  // latchDown.set(1);
-
-  // //position for driver 
-  // moveForward(-20, 20, 10);      
-  // turnClockwise(35, 20, 10);
-
+  //go to remove red from corner
+  moveForward(-10, 20, 10);
+  liftAuton(-120, 10, 10);   //push lift to the bottom
+  turnClockwise(-20, 20, 10); 
+  moveForward(100, 100, 10);
+  latchDown.set(0);
+  moveForward(-120, 100, 10);
+  turnClockwise(20, 20, 10);
+  liftAuton(240, 50, 5);
+  moveForward(10, 20, 10);
+  liftAuton(-2, 20, 5);
+  wait(1, sec);
+  latchDown.set(1);
 
 //-------------------------------------------
  //ORIGINAL CODE HERE
